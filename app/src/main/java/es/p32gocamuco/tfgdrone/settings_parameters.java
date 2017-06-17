3162b521e946374ef7a59c3c30610c1f43350ce5
Metichi <metichi@gmail.com>
2017-06-18 01:27:57 +0200

    Corrección y cosas
    Cambiado el menu de ajustes para que sólo se pueda introducir un número en la altura por defecto.
    
    Se ha intentado crear una clase que almacene los valores de la configuración.

---
+++ app/src/main/java/es/p32gocamuco/tfgdrone/settings_parameters.java
@@ -0,0 +1,13 @@
+package es.p32gocamuco.tfgdrone;
+
+/**
+ * Created by Manuel Gómez Castro on 17/06/2017.
+ */
+
+public class settings_parameters {
+    //Esta clase se utiliza para almacenar en memoria los valores de los ajustes.
+    private float altura;
+    private float velocidad_maxima;
+    private int LP_comportamiento;
+    private int LS_comportamiento;
+}
