79c8fdf67f533dfce8438b9100517829e7b97968
Metichi <metichi@gmail.com>
2017-06-18 17:04:27 +0200

    Añadido menú de selección de batería crítica
    Comentada la clase Settings_parameters

--- app/src/main/java/es/p32gocamuco/tfgdrone/settings_parameters.java
+++ app/src/main/java/es/p32gocamuco/tfgdrone/settings_parameters.java
@@ -1,7 +1,14 @@
 package es.p32gocamuco.tfgdrone;
 
-/**
- * Created by Manuel Gómez Castro on 17/06/2017.
+/*La clase Settings_Parameters se utilizará como raíz para las clases de rutas de vuelo.
+    Los parámetros configurables serán los siguientes:
+        -Velocidad máxima de vuelo: Determinada por las características del drone, se obtendrá del manual de instrucciones
+        -Velocidad definida de vuelo: Se aplicará un multiplicador a la velocidad máxima, 0.3, 0.6 y 0.9 para baja media y alta respectivamente.
+        -Acción en caso de batería baja. 1 = Retorno a home, 2 = Aterrizar inmediatamente, 3 = Control manual
+            Es posible que, en distintos puntos de la ruta, se busque una acción distinta. El valor por defecto será retorno a casa.
+        -Acción en caso de batería crítica
+        -Acción en caso de perdida de señal
+            Estas dos opciones no cuentan con control manual como respuesta
  */
 
 public class settings_parameters {
